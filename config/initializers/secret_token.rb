4f3076ef1a1bc37f35ad5c67b52c4d713756fe08
vadymkoch <vadymkoch@gmail.com>
2013-06-05 22:13:12 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fa::Application.config.secret_key_base = 'd6978aff10f0043db59c239cb3ca831a4e4c047e631163ce69fcc2f2f687cc8d91f335c5f74f98ac6b3d3b5390c88aa04ae3b2d5deeb120d7f54a5aeb8609648'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fa::Application.config.session_store :cookie_store, key: '_fa_session'
